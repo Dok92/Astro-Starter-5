0578cdffedb23a683181fd52ef1b37b8e3069e9a
Dok92 <doslovdok@gmail.com>
2026-02-17 17:35:47 +0100

    Updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
-import partytown from '@astrojs/partytown';
-
 export default defineConfig({
-  integrations: [
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
